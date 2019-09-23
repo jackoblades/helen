59f70ae4ca94821a3dd6a026baaab2ad2e5415f4
Dustin Walker <dustin.walker@email.wsu.edu>
2019-09-23 13:38:11 -0400

    initial project structure

---
+++ src/helen.term/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace helen.term
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
